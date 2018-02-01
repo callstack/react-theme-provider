caab0ed585a7a6680d4a38c24a23210b056e652a
Jakub Kłobus <souhepend@hotmail.com>
2018-02-01 09:08:26 +0100

    Added ThemeProvider and withTheme code

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { ThemeProvider as ThemeProvider } from '/ThemeProvider';
+export { withTheme as withTheme } from '/withTheme';
+export { createThemeProvider as createThemeProvider } from '/createThemeProvider';
