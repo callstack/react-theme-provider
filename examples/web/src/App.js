29daf971e5336da7d7b93a05777fec54425cfd17
Jakub Kłobus <souhepend@hotmail.com>
2018-02-05 13:32:35 +0100

    Use proper theme in example app

--- examples/web/src/App.js
+++ examples/web/src/App.js
@@ -9,35 +9,6 @@ import ThemeChanger from './ThemeChanger';
 import { ThemeProvider, themes } from './theming';
 import type { Theme } from './theming';
 
-// import type { ThemingType } from 'react-theme-provider'
-
-// type Theme = {
-//   primaryColor: string,
-//   accentColor: string,
-//   backgroundColor: string,
-//   textColor: string,
-//   secondaryColor: string,
-// };
-
-// const themes: { [key: string]: Theme } = {
-//   default: {
-//     primaryColor: '#FFA72A',
-//     accentColor: '#458622',
-//     backgroundColor: '#FFC777',
-//     textColor: '#504f4d',
-//     secondaryColor: '#7F5315',
-//   },
-//   dark: {
-//     primaryColor: '#FFA72A',
-//     accentColor: '#458622',
-//     backgroundColor: '#504f4d',
-//     textColor: '#FFC777',
-//     secondaryColor: '#252525',
-//   },
-// };
-
-// const { ThemeProvider, withTheme }: ThemingType<Theme> = createTheming(themes.default);
-
 class App extends React.Component<*, { theme: Theme }> {
   state = {
     theme: themes.default,
@@ -49,15 +20,7 @@ class App extends React.Component<*, { theme: Theme }> {
 
   render() {
     return (
-      <ThemeProvider
-        theme={{
-          primaryColor: '#FFA72A',
-          accentColor: '#458622',
-          backgroundColor: '#504f4d',
-          textColor: '#FFC777',
-          secondaryColor: '#252525',
-        }}
-      >
+      <ThemeProvider theme={this.state.theme}>
         <div>
           <Header />
           <ThemeChanger
