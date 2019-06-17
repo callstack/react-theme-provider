0498bcd12511adf66df52eddcb8992696812ec8e
Francesco Persico <francescopersico@gmail.com>
2019-06-17 13:55:57 +0200

    Use external hoist-non-react-statics types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,64 +2,7 @@
 // TypeScript version 3.0.3
 
 import * as React from 'react';
-
-// Static hoisting
-interface REACT_STATICS {
-  childContextTypes: true;
-  contextType: true;
-  contextTypes: true;
-  defaultProps: true;
-  displayName: true;
-  getDefaultProps: true;
-  getDerivedStateFromError: true;
-  getDerivedStateFromProps: true;
-  mixins: true;
-  propTypes: true;
-  type: true;
-}
-
-interface KNOWN_STATICS {
-  name: true;
-  length: true;
-  prototype: true;
-  caller: true;
-  callee: true;
-  arguments: true;
-  arity: true;
-}
-
-interface MEMO_STATICS {
-  $$typeof: true;
-  compare: true;
-  defaultProps: true;
-  displayName: true;
-  propTypes: true;
-  type: true;
-}
-
-interface FORWARD_REF_STATICS {
-  $$typeof: true;
-  render: true;
-  defaultProps: true;
-  displayName: true;
-  propTypes: true;
-}
-
-type NonReactStatics<
-  S extends React.ComponentType<any>,
-  C extends {
-    [key: string]: true;
-  } = {}
-> = {
-  [key in Exclude<
-    keyof S,
-    S extends React.MemoExoticComponent<any>
-      ? keyof MEMO_STATICS | keyof C
-      : S extends React.ForwardRefExoticComponent<any>
-      ? keyof FORWARD_REF_STATICS | keyof C
-      : keyof REACT_STATICS | keyof KNOWN_STATICS | keyof C
-  >]: S[key]
-};
+import hoistNonReactStatics = require('hoist-non-react-statics');
 
 type $Without<T, K> = Pick<T, Exclude<keyof T, K>>;
 type $DeepPartial<T> = { [P in keyof T]?: $DeepPartial<T[P]> };
@@ -71,7 +14,7 @@ export type ThemingType<Theme> = {
   ) => React.ComponentType<
     $Without<Props, 'theme'> & { theme?: $DeepPartial<Theme> }
   > &
-    NonReactStatics<typeof WrappedComponent>;
+    hoistNonReactStatics.NonReactStatics<typeof WrappedComponent>;
   useTheme(overrides?: $DeepPartial<Theme>): Theme;
 };
 
